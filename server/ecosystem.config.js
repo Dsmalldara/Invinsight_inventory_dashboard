bf2fc3be0c421b9c6060817b4fc7045a22133a46
Darasimi <topedarasimi5@gmail.com>
2024-11-14 18:18:36 +0100

    added changes to package.json

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -7,8 +7,6 @@ module.exports = {
             env: {
               NODE_ENV: "development",
               PORT: 8000,
-              FRONTEND_PORT: "http://localhost:3000",
-              DATABASE_URL:"postgresql://postgres.ftnlurapzxijrmtolrfr:Dadadarasimi1@aws-0-eu-west-2.pooler.supabase.com:5432/postgres?pgbouncer=true&connection_limit=1"
             }
         }
     ]
