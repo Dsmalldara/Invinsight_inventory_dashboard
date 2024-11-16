105cee53a470fe8f47c79e9ea092d471901e728e
Darasimi <topedarasimi5@gmail.com>
2024-11-16 20:39:35 +0100

    fixing bugs

--- server/src/index.ts
+++ server/src/index.ts
@@ -17,7 +17,9 @@ app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}))
 app.use(morgan("common"))
 app.use(cors({
     origin: process.env.FrontendPort || "http://localhost:3000",
-    credentials:true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:false}))
